704cfcd7d8805f0eef8fc9f64bcc6f64de054ed5
Lukas Stampf <e0725140@student.tuwien.ac.at>
2012-01-11 01:24:06 +0100

    add missing file

---
+++ smxb-credit-contract/src/main/java/at/ac/tuwien/infosys/aic11/services/contract/ContractManagementImpl.java
@@ -0,0 +1,57 @@
+package at.ac.tuwien.infosys.aic11.services.contract;
+
+import at.ac.tuwien.infosys.aic11.dto.CreditRequest;
+import at.ac.tuwien.infosys.aic11.dto.Offer;
+import at.ac.tuwien.infosys.aic11.legacy.exceptions.OfferDoesNotExistException;
+import at.ac.tuwien.infosys.aic11.legacy.interfaces.IContractManagementLegacy;
+
+public class ContractManagementImpl implements ContractManagement {
+	IContractManagementLegacy legacy;
+
+	public void setLegacy(IContractManagementLegacy legacy) {
+		this.legacy = legacy;
+	}
+
+	@Override
+	public Offer createOffer(CreditRequest creditRequest)
+			throws ContractManagementException {
+		if (validateCreditRequest(creditRequest)) {
+			return legacy.generateOffer(creditRequest);
+		} else {
+			throw new ContractManagementException(
+					"Validation of credit request failed!");
+		}
+	}
+
+	@Override
+	public void updateOffer(Offer offer) throws ContractManagementException {
+		try {
+			legacy.updateOffer(offer);
+		} catch (OfferDoesNotExistException e) {
+			throw new ContractManagementException(e);
+		}
+	}
+
+	private boolean validateCreditRequest(CreditRequest request) {
+		return (request.getDuration() != null) && (request.getMoney() != null)
+				&& (request.getCustomer() != null);
+	}
+
+	@Override
+	public void acceptOffer(Offer offer) throws ContractManagementException {
+		try {
+			legacy.acceptOffer(offer);
+		} catch (OfferDoesNotExistException e) {
+			throw new ContractManagementException(e);
+		}
+	}
+
+	@Override
+	public void declineOffer(Offer offer) throws ContractManagementException {
+		try {
+			legacy.declineOffer(offer);
+		} catch (OfferDoesNotExistException e) {
+			throw new ContractManagementException(e);
+		}
+	}
+}
